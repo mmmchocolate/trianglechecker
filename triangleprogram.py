ca77d6bbbbecf66f54069726512be5424c207141
mmmchocolate <ah7n17@soton.ac.uk>
2018-04-23 22:56:46 +0100

    CT draw function all working and implemented CT equations function all working and implemented

--- triangleprogram.py
+++ triangleprogram.py
@@ -13,7 +13,7 @@ ratcanvas.pack(side = TOP)
 rat_t = turtle.RawTurtle(ratcanvas)
 rat_t.hideturtle()
 rat_t.speed(0)
-equation_data = "Please enter data to the calculator"
+rat_equation_data, ct_equation_data = "Please enter data to the calculator", "Please enter data to the calculator",
 ctcanvas = Canvas(ct, width = 600, height = 400)
 ctcanvas.pack(side = TOP)
 
@@ -25,7 +25,7 @@ ct.withdraw()
 rat.withdraw()
 
 def rat_calculate():
-	global equation_data
+	global rat_equation_data
 	try:
 		A = float(A_in.get())
 		B = float(B_in.get())
@@ -39,7 +39,7 @@ def rat_calculate():
 			angle_B = round(math.degrees(math.atan(float(B)/float(C))),2)
 			angle_C = 90 - angle_B
 			rat_set(A,B,C,D,angle_B,angle_C)
-			draw(float(A),float(B),float(C),90,float(angle_B),float(angle_C))
+			rat_draw(float(A),float(B),float(C),90,float(angle_B),float(angle_C))
 			equation(True, 'Side A = sqrt({0}**2 + {1}**2)'.format(B,C), 'angle_B = atan(float({0})/float({1})'.format(angle_B,angle_C), 'angle C = 90 - {0}'.format(angle_B))
 
 		elif B == 0 and A > 0 and C > 0:
@@ -47,7 +47,7 @@ def rat_calculate():
 			angle_B = round(math.degrees(math.acos(float(C)/float(A))),2)
 			angle_C = 90 - angle_B
 			rat_set(A,B,C,D,angle_B,angle_C)
-			draw(float(A),float(B),float(C),90,float(angle_B),float(angle_C))
+			rat_draw(float(A),float(B),float(C),90,float(angle_B),float(angle_C))
 			equation(True, 'Side B = sqrt({0}**2 - {1}**2)'.format(A,B), 'angle_B = acos(float({0})/float({1})'.format(C,A), 'angle C = 90 - {0}'.format(angle_B))
 			
 		elif C == 0 and A > 0 and B > 0:
@@ -55,7 +55,7 @@ def rat_calculate():
 			angle_B = round(math.degrees(math.asin(float(B)/float(A))),2)
 			angle_C = 90 - angle_B
 			rat_set(A,B,C,D,angle_B,angle_C)
-			draw(float(A),float(B),float(C),90,float(angle_B),float(angle_C))
+			rat_draw(float(A),float(B),float(C),90,float(angle_B),float(angle_C))
 			equation(True, 'Side C = sqrt({0}**2 - {1}**2)'.format(A,B), 'angle_B = asin(float({0})/float({1})'.format(B,A), 'angle C = 90 - {0}'.format(angle_B))
 
 		elif E > 0 and A > 0: 
@@ -64,7 +64,7 @@ def rat_calculate():
 			C = round(float(A)*math.cos(math.radians(float(E))),2)
 			angle_B = E
 			rat_set(A,B,C,D,angle_B,angle_C)
-			draw(float(A),float(B),float(C),90,float(angle_B),float(angle_C))
+			rat_draw(float(A),float(B),float(C),90,float(angle_B),float(angle_C))
 			equation(True, 'Side B = {0}*sin({1})'.format(A,E), 'Side C = {0}*cos({1})'.format(A,E), 'angle_C = 90-{0}'.format(E))
 
 		elif E > 0 and B > 0:
@@ -73,7 +73,7 @@ def rat_calculate():
 			C = round(float(B)/math.tan(math.radians(float(E))),2)
 			angle_B = E
 			rat_set(A,B,C,D,angle_B,angle_C)
-			draw(float(A),float(B),float(C),90,float(angle_B),float(angle_C))
+			rat_draw(float(A),float(B),float(C),90,float(angle_B),float(angle_C))
 			equation(True, 'Side A = {0}*sin({1})'.format(B,E), 'Side C = {0}*tan({1})'.format(B,E), 'angle_C = 90-{0}'.format(E))
 
 		elif E > 0 and C > 0: 
@@ -82,7 +82,7 @@ def rat_calculate():
 			B = round(float(C)*math.tan(math.radians(float(E))),2)
 			angle_B = E
 			rat_set(A,B,C,D,angle_B,angle_C)
-			draw(float(A),float(B),float(C),90,float(angle_B),float(angle_C))
+			rat_draw(float(A),float(B),float(C),90,float(angle_B),float(angle_C))
 			equation(True, 'Side A = {0}*cos({1})'.format(C,E), 'Side B = {0}*tan({1})'.format(C,E), 'angle_C = 90-{0}'.format(E))
 
 		elif F > 0 and A > 0:
@@ -91,7 +91,7 @@ def rat_calculate():
 			C = round(float(A)*math.sin(math.radians(float(F))),2)
 			angle_C = F
 			rat_set(A,B,C,D,angle_B,angle_C)
-			draw(float(A),float(B),float(C),90,float(angle_B),float(angle_C))
+			rat_draw(float(A),float(B),float(C),90,float(angle_B),float(angle_C))
 			equation(True, 'Side B = {0}*cos({1})'.format(A,F), 'Side C = {0}*sin({1})'.format(A,F), 'angle B = 90-{0}'.format(F))
 
 		elif F > 0 and B > 0:
@@ -100,7 +100,7 @@ def rat_calculate():
 			C = round(float(B)*math.tan(math.radians(float(F))),2)
 			angle_C = F
 			rat_set(A,B,C,D,angle_B,angle_C)
-			draw(float(A),float(B),float(C),90,float(angle_B),float(angle_C))
+			rat_draw(float(A),float(B),float(C),90,float(angle_B),float(angle_C))
 			equation(True, 'Side A = {0}*cos({1})'.format(B,F), 'Side C = {0}*tan({1})'.format(B,F), 'angle B = 90-{0}'.format(F))
 
 		elif F > 0 and C > 0:
@@ -109,7 +109,7 @@ def rat_calculate():
 			B = round(float(C)/math.tan(math.radians(float(F))),2)
 			angle_C = F
 			rat_set(A,B,C,D,angle_B,angle_C)
-			draw(float(A),float(B),float(C),90,float(angle_B),float(angle_C))
+			rat_draw(float(A),float(B),float(C),90,float(angle_B),float(angle_C))
 			equation(True, 'Side A = {0}*sin({1})'.format(C,F), 'Side B = {0}*tan({1})'.format(C,F), 'angle B = 90-{0}'.format(F))
 		else:
 			messagebox.showinfo("Error", "Please enter two unknowns")
@@ -137,115 +137,173 @@ def ct_calculate():
 			angle_B = round(math.degrees(math.acos((C**2 + A**2 - B**2)/(2.0 * C * A))),2)
 			angle_C = round(math.degrees(math.acos((B**2 + A**2 - C**2)/(2.0 * B * A))),2)
 			ct_set(str(A),str(B),str(C),str(angle_A),str(angle_B),str(angle_C))
-			draw(A,B,C,angle_A,angle_B,angle_C)
+			ct_draw(A,B,C,angle_A,angle_B,angle_C)
+			equation(False, 'Angle A = acos({1}**2 + {2}**2 - {0}**2)/(2.0 * {1} * {2})'.format(A,B,C), 
+				'Angle B = acos({2}**2 + {0}**2 - {1}**2)/(2.0 * {2} * {0})'.format(A,B,C), 
+				'Angle C = acos(({1}**2 + {0}**2 - {2}**2)/(2.0 * {1} * {0})'.format(A,B,C))
 
 		elif (A) >0 and (B) > 0 and (D) > 0:
 			angle_B = round(math.degrees(math.asin((B*math.sin(math.radians(D)))/A)),2)
 			angle_C = 180 - D - angle_B
 			side_C = round(A*math.sin(math.radians(angle_C))/(math.sin(math.radians(D))),2)
 			ct_set(str(A),str(B),str(side_C),str(D),str(angle_B),str(angle_C))
+			ct_draw(A,B,side_C,D,angle_B,angle_C)
+			equation(False, 'Angle B = asin({0}*sin(({1})/{2})'.format(B,D,A), 'Angle C = 180 - {0} - {1}'.format(D,angle_B), 
+				'Side C = {0}sin({1})/(sin({2})'.format(A,angle_C,D))
 
 		elif (A) >0 and (B) > 0 and (E) > 0:
 			angle_A = round(math.degrees(math.asin((A*math.sin(math.radians(E)))/B)),2)
 			angle_C = 180 - E - angle_A
 			side_C = round(A*math.sin(math.radians(angle_C))/(math.sin(math.radians(angle_A))),2)
 			ct_set(str(A),str(B),str(side_C),str(angle_A),str(E),str(angle_C))
+			ct_draw(A,B,side_C,angle_A,E,angle_C)
+			equation(False, 'Angle A = asin({0}*sin(({1})/{2})'.format(A,E,B), 'Angle C = 180 - {0} - {1}'.format(E,angle_A), 
+				'Side C = {0}sin({1})/(sin({2})'.format(A,angle_C,angle_A))
 
 		elif (A) >0 and (B) > 0 and (F) > 0:
 			side_C = round(math.sqrt((A**2 + B**2)-2*A*B*math.cos(math.radians(F))),2)
 			angle_A = round(math.degrees(math.acos((B**2 + side_C**2 - A**2)/(2.0 * B *side_C))),2)
 			angle_B = round(math.degrees(math.acos((A**2 + side_C**2 - B**2)/(2.0 * A *side_C))),2)
 			ct_set(str(A),str(B),str(side_C),str(angle_A),str(angle_B),str(F))
+			ct_draw(A,B,side_C,angle_A,angle_B,F)
+			equation(False, 'Side C = sqrt({0}**2 + {1}**2)-2*{0}*{1}*math.cos({2})'.format(A,B,F),
+				'Angle A = acos({1}**2 + {2}**2 - {0}**2)/(2.0 * {1} * {2})'.format(A,B,C), 
+				'Angle B = acos({2}**2 + {0}**2 - {1}**2)/(2.0 * {2} * {0})'.format(A,B,C))
 
 		elif (A) >0 and (C) > 0 and (D) > 0:
 			angle_C = round(math.degrees(math.asin((C*math.sin(math.radians(D)))/A)),2)
 			angle_B = 180 - D - angle_C
 			side_B = round(A*math.sin(math.radians(angle_B))/(math.sin(math.radians(D))),2)
 			ct_set(str(A),str(side_B),str(C),str(D),str(angle_B),str(angle_C))
+			ct_draw(A,side_B,C,D,angle_B,angle_C)
+			equation(False, 'Angle C = asin({0}*sin(({1})/{2})'.format(C,D,A), 'Angle B = 180 - {0} - {1}'.format(D,angle_C), 
+				'Side B = {0}sin({1})/(sin({2})'.format(A,angle_B,D))
 			
 		elif (A) >0 and (C) > 0 and (E) > 0:
 			side_B = round(math.sqrt((A**2 + C**2)-2*A*C*math.cos(math.radians(E))),2)
 			angle_A = round(math.degrees(math.acos((side_B**2 + C**2 - A**2)/(2.0 * side_B *C))),2)
 			angle_C = round(math.degrees(math.acos((side_B**2 + A**2 - C**2)/(2.0 * side_B *A))),2)
 			ct_set(str(A),str(side_B),str(C),str(angle_A),str(E),str(angle_C))
+			ct_draw(A,side_B,C,angle_A,E,angle_C)
+			equation(False, 'Side B = sqrt({0}**2 + {1}**2)-2*{0}*{1}*cos({2})'.format(A,C,E), 
+				'Angle A = acos({1}**2 + {2}**2 - {0}**2)/(2.0 * {1} * {2})'.format(A,side_B,C), 
+				'Angle C = acos(({1}**2 + {0}**2 - {2}**2)/(2.0 * {1} * {0})'.format(A,side_B,C))
 			
 		elif (A) >0 and (C) > 0 and (F) > 0:
 			angle_A = round(math.degrees(math.asin((A*math.sin(math.radians(F)))/C)),2)
 			angle_B = 180 - F - angle_A
 			side_B = round(C*math.sin(math.radians(angle_B))/(math.sin(math.radians(F))),2)
 			ct_set(str(A),str(side_B),str(C),str(angle_A),str(angle_B),str(F))
+			ct_draw(A,side_B,C,angle_A,angle_B,F)
+			equation(False, 'Angle A = asin({0}*sin(({1})/{2})'.format(A,F,C), 'Angle B = 180 - {0} - {1}'.format(F,angle_A), 
+				'Side B = {0}sin({1})/(sin({2})'.format(C,angle_B,F))
 
 		elif (B) >0 and (C) > 0 and (D) > 0:
 			side_A = round(math.sqrt((B**2 + C**2)-2*B*C*math.cos(math.radians(D))),2)
 			angle_B = round(math.degrees(math.acos((side_A**2 + C**2 - B**2)/(2.0 * side_A * C))),2)
 			angle_C = round(math.degrees(math.acos((side_A**2 + B**2 - C**2)/(2.0 * side_A * B))),2)
 			ct_set(str(side_A),str(B),str(C),str(D),str(angle_B),str(angle_C))
+			ct_draw(side_A,B,C,D,angle_B,angle_C)
+			equation(False, 'Side A = sqrt({0}**2 + {1}**2)-2*{0}*{1}*cos({2})'.format(B,C,D), 
+				'Angle B = acos({1}**2 + {2}**2 - {0}**2)/(2.0 * {1} * {2})'.format(B,side_A,C), 
+				'Angle C = acos(({1}**2 + {0}**2 - {2}**2)/(2.0 * {1} * {0})'.format(B,side_A,C))
 			
 		elif (B) >0 and (C) > 0 and (E) > 0:
 			angle_C = round(math.degrees(math.asin((C*math.sin(math.radians(E)))/B)),2)
 			angle_A = 180 - E - angle_C
 			side_A = round(B*math.sin(math.radians(angle_A))/(math.sin(math.radians(E))),2)
 			ct_set(str(side_A),str(B),str(C),str(angle_A),str(E),str(angle_C))
+			ct_draw(side_A,B,C,angle_A,E,angle_C)
+			equation(False, 'Angle C = asin({0}*sin(({1})/{2})'.format(C,E,B), 'Angle A = 180 - {0} - {1}'.format(E,angle_C), 
+				'Side A = {0}sin({1})/(sin({2})'.format(B,angle_A,E))
 		
 		elif (B) >0 and (C) > 0 and (F) > 0:
 			angle_B = round(math.degrees(math.asin((B*math.sin(math.radians(F)))/C)),2)
 			angle_A = 180 - F - angle_B
 			side_A = round(B*math.sin(math.radians(angle_A))/(math.sin(math.radians(angle_B))),2)
-			ct_set(str(side_A),str(B),str(C),str(angle_A),str(angle_B),str(F))	
+			ct_set(str(side_A),str(B),str(C),str(angle_A),str(angle_B),str(F))
+			ct_draw(side_A,B,C,angle_A,angle_B,F)
+			equation(False, 'Angle B = asin({0}*sin(({1})/{2})'.format(B,F,C), 'Angle A = 180 - {0} - {1}'.format(F,angle_B), 
+				'Side A = {0}sin({1})/(sin({2})'.format(B,angle_A,angle_B))
 
 		elif (D) >0 and (E) > 0 and (A) > 0:
 			angle_C = 180 - D - E
 			side_B = round(A*math.sin(math.radians(E))/(math.sin(math.radians(D))),2)	
 			side_C = round(A*math.sin(math.radians(angle_C))/(math.sin(math.radians(D))),2)	
 			ct_set(str(A),str(side_B),str(side_C),str(D),str(E),str(angle_C))
+			ct_draw(A,side_B,side_C,D,E,angle_C)
+			equation(False,'Angle C = 180 - {0} - {1}'.format(D,E), 'Side B = {0}*sin({1})/(sin({2})'.format(A,E,D), 
+				'Side C = {0}*sin({1})/(sin({2})'.format(A,angle_C,D))
 			
 		elif (D) >0 and (E) > 0 and (B) > 0:
 			angle_C = 180 - D - E
 			side_A = round(B*math.sin(math.radians(D))/(math.sin(math.radians(E))),2)	
 			side_C = round(B*math.sin(math.radians(angle_C))/(math.sin(math.radians(E))),2)	
 			ct_set(str(side_A),str(B),str(side_C),str(D),str(E),str(angle_C))
+			ct_draw(side_A,B,side_C,D,E,angle_C)
+			equation(False,'Angle C = 180 - {0} - {1}'.format(D,E), 'Side A = {0}*sin({1})/(sin({2})'.format(B,D,E), 
+				'Side C = {0}*sin({1})/(sin({2})'.format(B,angle_C,E))
 			
 		elif (D) >0 and (E) > 0 and (C) > 0:
 			angle_C = 180 - D - E
 			side_A = round(C*math.sin(math.radians(D))/(math.sin(math.radians(angle_C))),2)	
 			side_B = round(C*math.sin(math.radians(E))/(math.sin(math.radians(angle_C))),2)	
 			ct_set(str(side_A),str(side_B),str(C),str(D),str(E),str(angle_C))
+			ct_draw(side_A,side_B,C,D,E,angle_C)
+			equation(False,'Angle C = 180 - {0} - {1}'.format(D,E), 'Side A = {0}*sin({1})/(sin({2})'.format(C,D,angle_C), 
+				'Side C = {0}*sin({1})/(sin({2})'.format(C,E,angle_C))
 		
 		elif (D) >0 and (F) > 0 and (A) > 0:
 			angle_B = 180 - D - F
 			side_B = round(A*math.sin(math.radians(angle_B))/(math.sin(math.radians(D))),2)	
 			side_C = round(A*math.sin(math.radians(F))/(math.sin(math.radians(D))),2)	
 			ct_set(str(A),str(side_B),str(side_C),str(D),str(angle_B),str(F))
+			ct_draw(A,side_B,side_C,D,angle_B,F)
+			equation(False,'Angle B = 180 - {0} - {1}'.format(D,F), 'Side B = {0}*sin({1})/(sin({2})'.format(A,angle_B,D), 
+				'Side C = {0}*sin({1})/(sin({2})'.format(A,F,D))
 		
 		elif (D) >0 and (F) > 0 and (B) > 0:
 			angle_B = 180 - D - F
 			side_A = round(B*math.sin(math.radians(D))/(math.sin(math.radians(angle_B))),2)	
 			side_C = round(B*math.sin(math.radians(F))/(math.sin(math.radians(angle_B))),2)	
 			ct_set(str(side_A),str(B),str(side_C),str(D),str(angle_B),str(F))
+			ct_draw(side_A,B,side_C,D,angle_B,F)
+			equation(False,'Angle B = 180 - {0} - {1}'.format(D,F), 'Side A = {0}*sin({1})/(sin({2})'.format(B,D,angle_B), 
+				'Side C = {0}*sin({1})/(sin({2})'.format(B,F,angle_B))
 			
 		elif (D) >0 and (F) > 0 and (C) > 0:
 			angle_B = 180 - D - F
 			side_A = round(C*math.sin(math.radians(D))/(math.sin(math.radians(F))),2)	
 			side_B = round(C*math.sin(math.radians(angle_B))/(math.sin(math.radians(F))),2)	
 			ct_set(str(side_A),str(side_B),str(C),str(D),str(angle_B),str(F))
+			ct_draw(side_A,side_B,C,D,angle_B,F)
+			equation(False,'Angle B = 180 - {0} - {1}'.format(D,F), 'Side A = {0}*sin({1})/(sin({2})'.format(C,D,F), 
+				'Side B = {0}*sin({1})/(sin({2})'.format(C,angle_B,F))
 	
 		elif (E) >0 and (F) > 0 and (A) > 0:
 			angle_A = 180 - E - F
 			side_B = round(A*math.sin(math.radians(E))/(math.sin(math.radians(angle_A))),2)	
 			side_C = round(A*math.sin(math.radians(F))/(math.sin(math.radians(angle_A))),2)	
 			ct_set(str(A),str(side_B),str(side_C),str(angle_A),str(E),str(F))
+			ct_draw(A,side_B,side_C,angle_A,E,F)
+			equation(False,'Angle B = 180 - {0} - {1}'.format(E,F), 'Side B = {0}*sin({1})/(sin({2})'.format(A,E,angle_A), 
+				'Side C = {0}*sin({1})/(sin({2})'.format(A,F,angle_A))
 			
 		elif (E) >0 and (F) > 0 and (B) > 0:
 			angle_A = 180 - E - F
 			side_A = round(B*math.sin(math.radians(angle_A))/(math.sin(math.radians(E))),2)
 			side_C = round(B*math.sin(math.radians(F))/(math.sin(math.radians(E))),2)
 			ct_set(str(side_A),str(B),str(side_C),str(angle_A),str(E),str(F))
+			ct_draw(side_A,B,side_C,angle_A,E,F)
+			equation(False,'Angle A = 180 - {0} - {1}'.format(E,F), 'Side A = {0}*sin({1})/(sin({2})'.format(B,angle_A,E), 
+				'Side C = {0}*sin({1})/(sin({2})'.format(C,F,E))
 		
 		elif (E) >0 and (F) > 0 and (C) > 0:
 			angle_A = 180 - E - F
 			side_A = round(C*math.sin(math.radians(angle_A))/(math.sin(math.radians(F))),2)
 			side_B = round(C*math.sin(math.radians(E))/(math.sin(math.radians(F))),2)
 			ct_set(str(side_A),str(side_B),str(C),str(angle_A),str(E),str(F))
+			ct_draw(side_A,side_B,C,angle_A,E,F)
 		else:
 			messagebox.showinfo("Error", "Please enter three unknowns")
 
@@ -324,7 +382,7 @@ def goto_rat():
 	ct.withdraw()
 	rat.deiconify()
 
-def draw(a,b,c,d,e,f):
+def rat_draw(a,b,c,d,e,f):
 	rat_t.clear()
 	rat_t.speed(0)
 	rat_t.up()
@@ -396,21 +454,113 @@ def triangle_label(a,b,c,d,e,f):
 	
 	rat_t.forward(y)
 
+def ct_draw(a,b,c,d,e,f):
+	ct_t.clear()
+	ct_t.speed(0)
+	ct_t.up()
+	ct_t.goto(0,-150)
+	# rat_t.write("Home = ", True, align="center", font=("Arial", 10, "normal"))
+	ct_t.penup()
+
+	if b > c :
+		hyp = (300/b) * a
+		base = (300/b) *b
+		opp = (300/b) * c
+	elif c > b or b == c:
+		hyp = (300/c) * a
+		base = (300/c) *b
+		opp = (300/c) * c
+	
+	x = base/2
+	ct_t.pendown()
+	ct_t.forward(x) 
+	ct_t.left(180-d)
+	ct_t.forward(opp)
+	ct_t.left(180-e)
+	ct_t.forward(hyp)
+	ct_t.left(180-f)
+	ct_t.forward(x)
+	ct_t.penup()
+	ct_triangle_label(hyp,base,opp,d,e,f)
+
+def ct_triangle_label(a,b,c,d,e,f):
+	if a > b and a > c:
+		t = '''
+		Hypotenuse
+		  Side A
+		'''
+		t2 = '''
+		Opposite
+		  Side C
+		'''
+		t3 = '''
+		Adjacent
+		  Side B
+		'''
+	else:
+		t = '''
+		Opposite
+		  Side A
+		'''
+		t2 = '''
+		Hypotenuse
+		  Side C
+		'''
+		t3 = '''
+		Adjacent
+		  Side B
+		'''
+	t4 = 'hello'
+	x = a/2
+	y = b/2
+	q = b/4
+	z = c/2
+	ct_t.goto(0,-170)
+	ct_t.penup()
+	ct_t.right(90)
+	ct_t.forward(30)
+	ct_t.write(t3 ,False, align="center", font=("Arial", 10, "normal"))
+	ct_t.backward(30)
+	ct_t.left(90)
+	ct_t.forward(y)
+	ct_t.write("Angle A", False, align="center", font=("Arial", 10, "normal"))
+	ct_t.left(180-d)
+	ct_t.forward(z)
+	ct_t.write(t2, False, align="center", font=("Arial", 10, "normal"))
+	ct_t.forward(z+30)
+	# ct_t.forward(z+30)
+	ct_t.write("Angle B", False, align="center", font=("Arial", 10, "normal"))
+	ct_t.left(180-e)
+	ct_t.forward(x)
+	ct_t.write(t, False, align="center", font=("Arial", 10, "normal"))
+	ct_t.forward(x+30)
+	ct_t.write("Angle C", False, align="center", font=("Arial", 10, "normal"))
+	ct_t.left(180-f)
+	ct_t.forward(y)
+
 def gtfo():
 	top.destroy()
 
 def equation(condition,x,y,z):
-	global equation_data
-	if condition:
-		equation_data = '''{0}
+	global rat_equation_data,ct_equation_data
+	if condition == True:
+		rat_equation_data = '''{0}
+						       {1}
+						   	{2}'''.format(x,y,z)
+	elif condition == False:
+		ct_equation_data = '''{0}
 						       {1}
 						   	{2}'''.format(x,y,z)
 
-def show_equations():
-	global equation_data
-	messagebox.showinfo("Equations", "{0}".format(equation_data))
-	equation_data = "Please enter data to the calculator"
+def rat_show_equations():
+	global rat_equation_data
+	messagebox.showinfo("Equations", "{0}".format(rat_equation_data))
+	rat_equation_data = "Please enter data to the calculator"
 
+def ct_show_equations():
+	global ct_equation_data
+	messagebox.showinfo("Equations", "{0}".format(ct_equation_data))
+	ct_equation_data = "Please enter data to the calculator"
 
 box0 = Frame(top)
 box0.pack()
@@ -457,8 +607,8 @@ cal_button = Button(box,text = "Calculate", command = lambda:rat_calculate())
 cal_button.grid(row = 5, column = 2)
 clear_botton = Button(box,text = "Clear", command = clear)
 clear_botton.grid(row = 6, column = 2)
-close_button = Button(box, text = "Equations", command = lambda:show_equations())
-close_button.grid(row = 7, column = 2)
+rat_equation_button = Button(box, text = "Equations", command = lambda:rat_show_equations())
+rat_equation_button.grid(row = 7, column = 2)
 change_button = Button(box, text = "Cosine Triangle", command = goto_ct)
 # change_button.grid(row = 8, column = 2)
 home_button = Button(box, text = "Home", command = home)
@@ -498,8 +648,8 @@ cal_button = Button(box1,text = "Calculate", command = lambda:ct_calculate())
 cal_button.grid(row = 5, column = 2)
 clear_botton = Button(box1,text = "Clear", command = clear)
 clear_botton.grid(row = 6, column = 2)
-close_button = Button(box1, text = "Close", command = gtfo)
-# close_button.grid(row = 7, column = 2)
+ct_equation_button = Button(box1, text = "Equations", command = lambda:ct_show_equations())
+ct_equation_button.grid(row = 7, column = 2)
 change_button = Button(box1, text = "Right angle triange", command = goto_rat)
 # change_button.grid(row = 8, column = 2)
 home_button = Button(box1, text = "Home", command = home)
